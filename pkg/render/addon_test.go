43afbff732ed197fb77d5eb9d0e474c8c85fcb0f
Gaurav Mehta <gaurav.mehta@suse.com>
2024-07-09 13:53:29 +1000

    generate templates from version_info

--- pkg/render/addon_test.go
+++ pkg/render/addon_test.go
@@ -1,9 +1,11 @@
 package render
 
 import (
-	"github.com/stretchr/testify/require"
 	"os"
+	"path/filepath"
 	"testing"
+
+	"github.com/stretchr/testify/require"
 )
 
 func Test_Addon(t *testing.T) {
@@ -12,6 +14,24 @@ func Test_Addon(t *testing.T) {
 	tmpPath, err := os.MkdirTemp(tmpDir, "rendered")
 	assert.NoError(err)
 	defer os.RemoveAll(tmpPath)
-	err = Addon(relativeTemplatePath, tmpPath)
+	err = Addon(relativeTemplatePath, tmpPath, filepath.Join(relativeVersionFilePath, defaultVersionFile))
+	assert.NoError(err)
+}
+
+func Test_generate_version_info_map(t *testing.T) {
+	assert := require.New(t)
+	result, err := generate_version_info_map(filepath.Join(relativeVersionFilePath, defaultVersionFile))
+	assert.NoError(err, "expected no error while reading version info map")
+	_, ok := result["VM_IMPORT_CONTROLLER_CHART_VERSION"]
+	assert.True(ok, "expected to find key VM_IMPORT_CONTROLLER_CHART_VERSION")
+}
+
+func Test_Template(t *testing.T) {
+	assert := require.New(t)
+	tmpDir := os.TempDir()
+	tmpPath, err := os.MkdirTemp(tmpDir, "template")
+	assert.NoError(err, "expected no error during tmp dir creation")
+	defer os.RemoveAll(tmpPath)
+	err = Template(relativeTemplatePath, tmpPath, filepath.Join(relativeVersionFilePath, defaultVersionFile))
 	assert.NoError(err)
 }
